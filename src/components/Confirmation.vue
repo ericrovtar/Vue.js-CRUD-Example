8e9137fdb42aaff69d518c597d1439b079c3c844
Eric Rovtar <eric.rovtar@me.com>
2017-02-05 15:44:30 -0500

    Add transition effect to confirmation

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -1,9 +1,11 @@
 <template>
-    <div v-if="confirmation.status !== '' && confirmation.status !== null"
-        class="add-bottom-margin add-padding" 
-        :class="confirmation.status === 'error' ? 'box--red' : 'box--green'">
-        {{ confirmation.message }}
-    </div>
+    <transition name="fade">
+        <div v-if="confirmation.status !== '' && confirmation.status !== null"
+            class="add-bottom-margin add-padding" 
+            :class="confirmation.status === 'error' ? 'box--red' : 'box--green'">
+            {{ confirmation.message }}
+        </div>
+    </transition>
 </template>
 
 <script>
